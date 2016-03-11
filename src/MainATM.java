deca8d9391c425f242e65f6241ae95095ca2d482
TAMP31 <mahadikaandre@gmail.com>
2016-03-11 04:08:57 -0800

    Membuat class mainATM dan membuat masukkan untk pin atm

---
+++ src/MainATM.java
@@ -0,0 +1,13 @@
+package pratikum2;
+import java.util.Scanner;
+public class MainATM {
+    public static void main(String[] args) {
+       Scanner in = new Scanner (System.in);
+       int menu, pin, trk, pinn2, trans;
+        System.out.println("Masukkan pin anda: ");
+        pin = in.nextInt();
+        ATM x1 = new ATM(5000000);
+        
+    }
+    
+}
