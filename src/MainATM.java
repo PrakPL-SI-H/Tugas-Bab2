d2a48840fff6514d492fc9f78dd451acc9ad9c6a
TAMP31 <mahadikaandre@gmail.com>
2016-03-11 04:25:31 -0800

    membuat case 1 dan case 2

--- src/MainATM.java
+++ src/MainATM.java
@@ -8,11 +8,32 @@ public class MainATM {
         pin = in.nextInt();
         ATM x1 = new ATM(11000000);
         do{
-            System.out.println(====Menu ATM====);
-            System.out.println(1. Cek Saldo);
-            System.out.println(2. Tarik Tunai);
-            System.out.println(3. Tranfer);
-            System.out.println(0. Keluar)
+            System.out.println("====Menu ATM====");
+            System.out.println("1. Cek Saldo");
+            System.out.println("2. Tarik Tunai");
+            System.out.println("3. Tranfer");
+            System.out.println("0. Keluar");
+            System.out.println("");
+            System.out.println("Pilihan : ");
+            menu = in.nextInt();
+        switch(menu){
+            case 1:
+                System.out.println("Jumlah saldo anda : "+x1.saldo);
+                break;
+            case 2:
+                System.out.println("Masukkan jumlah uang yang ingin diarik : ");
+                trk = in.nextInt();
+                if(trk<=x1.saldo){
+                x1.tarik(trk);
+                    System.out.println("Sisa saldo anda : "+x1.saldo);
+                }
+                else{
+                    System.out.println("Penarikan salah!");
+                    System.out.println("Saldo anda kurang dari "+trk);
+                }break;
+            case 3:
+                
+        
         }
     }
     
