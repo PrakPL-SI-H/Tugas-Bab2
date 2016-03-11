bb64a90f788a86d47721a08afe4aa8b912384bee
TAMP31 <mahadikaandre@gmail.com>
2016-03-11 05:22:18 -0800

    menyelesaikan method dan program selesai

--- src/MainATM.java
+++ src/MainATM.java
@@ -2,12 +2,12 @@ package pratikum2;
 import java.util.Scanner;
 public class MainATM {
     public static void main(String[] args) {
-       Scanner in = new Scanner (System.in);
-       int menu, pin, trk, pinn2, trans;
+        Scanner in = new Scanner(System.in);
+        int menu, pin, trk, pin2, trans;
         System.out.println("Masukkan pin anda: ");
         pin = in.nextInt();
         ATM x1 = new ATM(11000000);
-        do{
+        do {
             System.out.println("====Menu ATM====");
             System.out.println("1. Cek Saldo");
             System.out.println("2. Tarik Tunai");
@@ -16,25 +16,40 @@ public class MainATM {
             System.out.println("");
             System.out.println("Pilihan : ");
             menu = in.nextInt();
-        switch(menu){
-            case 1:
-                System.out.println("Jumlah saldo anda : "+x1.saldo);
-                break;
-            case 2:
-                System.out.println("Masukkan jumlah uang yang ingin diarik : ");
-                trk = in.nextInt();
-                if(trk<=x1.saldo){
-                x1.tarik(trk);
-                    System.out.println("Sisa saldo anda : "+x1.saldo);
-                }
-                else{
-                    System.out.println("Penarikan salah!");
-                    System.out.println("Saldo anda kurang dari "+trk);
-                }break;
-            case 3:
-                
-        
-        }
+            switch (menu) {
+                case 1:
+                    System.out.println("Jumlah saldo anda : " + x1.saldo);
+                    break;
+                case 2:
+                    System.out.println("Masukkan jumlah uang yang ingin diarik : ");
+                    trk = in.nextInt();
+                    if (trk <= x1.saldo) {
+                        x1.tarik(trk);
+                        System.out.println("Sisa saldo anda : " + x1.saldo);
+                    } else {
+                        System.out.println("Penarikan salah!");
+                        System.out.println("Saldo anda kurang dari " + trk);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Transfer ke rekening : ");
+                    pin2 = in.nextInt();
+                    System.out.println("Jumlah transfer : ");
+                    trans = in.nextInt();
+                    if (trans <= x1.saldo) {
+                        x1.transfer(trans);
+                        System.out.println("Sisa saldo : " + x1.saldo);
+                    } else {
+                        System.out.println("Tranfer gagal !");
+                        System.out.println("Saldo tidak mencukupi untuk melakukan transfer sebesar Rp. " + trans);
+                    }
+                    break;
+                case 0:
+                    System.out.println("Keluar. Terima Kasih");
+                    break;
+                default:
+                    System.out.println("Menu tidak ada, coba lagi");
+            }
+        } while (menu != 0);
     }
-    
 }
