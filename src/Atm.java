dbc40ff971785c5793f03d2f5039d34e84d91ef7
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-03-13 00:22:51 +0700

    edit method class final

--- src/Atm.java
+++ src/Atm.java
@@ -9,19 +9,26 @@ public class Atm {
     int cekSaldo;
     int Transfer;
     int pin;
-    int pin2;
+    int pil;
     Scanner ni = new Scanner(System.in);
-
+    
+    public int Jumlah(){
+        return Jumlah;
+    }
     public void ambilUang(){
-        System.out.println("Pilih Uang yang akan diambil");
+        System.out.println("============***===============");
+        System.out.println(" Pilih Uang yang akan diambil");
+        System.out.println("==============================");
         System.out.println("1.Rp 50.000");
         System.out.println("2.Rp 100.000");
         System.out.println("3.Rp 500.000");
         System.out.println("4.Rp 1.000.000");
         System.out.println("5.Rp 5.000.000");
         System.out.println("6.Rp 10.000.000");
-        System.out.println("Masukkan Pilihan anda = ");
+        System.out.println("==============================");
+        System.out.print("Masukkan Pilihan anda = ");
         int uang= ni.nextInt();
+        System.out.println("==============================");
         switch(uang){
             case 1 :Jumlah = Saldo - 50000;
                     System.out.println("Uang yang diambil Rp 50.000");
@@ -42,55 +49,73 @@ public class Atm {
                     System.out.println("Uang yang diambil Rp 10.000.000");
                     ;break;
             default:System.out.println("Pilihan Yang anda Masukkan Tidak Tersedia !!!");
-        }
+            System.out.println("==============================");
+            }
         }
         public void akSaldo(){
-            System.out.println("Saldo Awal anda tinggal = "+ Saldo);
-            System.out.println("Saldo Akhir anda tinggal = "+ Jumlah);
+            System.out.println("============***===============");
+            System.out.println("        Cek Saldo");
+            System.out.println("==============================");
+            System.out.println("Saldo awal anda tinggal  = "+ Saldo);
+            System.out.println("Saldo akhir anda tinggal = "+ Jumlah);
+            System.out.println("==============================");
         }
         public void Transfer(){
-            System.out.println("Transfer");
+            System.out.println("============***===============");
+            System.out.println("          Transfer");
+            System.out.println("==============================");
             System.out.println("1.Bank Yang Sama");
             System.out.println("2.Bank Lainnya");
-            System.out.println("Masukkan Pilihan Anda = ");
+            System.out.println("==============================");
+            System.out.print("Masukkan Pilihan Anda = ");
             int pilih =ni.nextInt();
-            if (pilih==1){
-                System.out.println("Masukkan Pin = ");
+            System.out.println("==============================");
+            if (pilih == 1){
+                System.out.print("Masukkan Pin = ");
                 pin = ni.nextInt();
-                System.out.println("Masukkan uang = ");
-                int no =ni.nextInt();
-                if (pin==1234){
-                    System.out.println("No Rekening : "+pin);
-                    System.out.println("An Nama     : Pak dhe Qu");
-                    System.out.println("Nominal     : "+no);
+                if (pin == 1111){
+                    System.out.println("No Rekening  : "+pin);
+                    System.out.println("An Nama      : Pak dhe Qu");
+                    System.out.print("Masukkan uang : ");
+                    int no =ni.nextInt();
+                    Jumlah = Saldo - no;
                     System.out.println("Transfer Sukses !!!");
-                }else 
+                }else{ 
                     System.out.println("Pin Tidak tersedia ");      
                     System.out.println("Transfer Gagal !!!");
-            }else if(pilih==2){
-                System.out.println("Masukkan Nominalnya = ");
-                int nom = ni.nextInt();
-                Jumlah=Saldo - nom;
-                System.out.println("Kode Bank");
-                System.out.println("01 Bank ini");
-                System.out.println("02 Bank mu");
-                System.out.println("03 Bank Negara");
-                System.out.println("Masukkan kode bank diikuti nomer rekening = ");
-                pin2 = ni.nextInt();
-                if (pin2 == 01222){
-                    System.out.println("No Rekening : "+pin2);
-                    System.out.println("An Nama     : Bpk Qu");
-                    System.out.println("Nominal     : "+nom);
+                }
+            }
+            else if(pilih == 2){
+                System.out.println("Bank");
+                System.out.println("No Nama Bank   Kode");
+                System.out.println("1. Bank ini     01");
+                System.out.println("2. Bank mu      02");
+                System.out.println("3. Bank Negara  03");
+                System.out.print("Masukkan urutan kode bank = ");
+                pil = ni.nextInt();
+                if (pil == 1){
+                    System.out.print("No Rekening (kode_No Rekening) : 01");
+                    int rek =ni.nextInt();
+                    System.out.println("An Nama                        : Bpk Qu");
+                    System.out.print("Masukkan Nominalnya            : ");
+                    int nom = ni.nextInt();
+                    Jumlah=Saldo - nom;
                     System.out.println("Transfer Sukses !!!");
-                }else if(pin2 == 02333){
-                    System.out.println("No Rekening : "+pin2);
-                    System.out.println("An Nama     : Ibu Qu");
-                    System.out.println("Nominal     : "+nom);
+                }else if(pil == 2){
+                    System.out.print("No Rekening (kode_No Rekening) : 02");
+                    int rek =ni.nextInt();
+                    System.out.println("An Nama                        : Ibu Qu");
+                    System.out.print("Masukkan Nominalnya            : ");
+                    int nom = ni.nextInt();
+                    Jumlah=Saldo - nom;
                     System.out.println("Transfer Sukses !!!");
-                }else if(pin2 == 03444){
-                    System.out.println("No Rekening : "+pin2);
-                    System.out.println("An Nama     : Adik Qu");
-                    System.out.println("Nominal     : "+nom);
+                }else if(pil == 3){
+                    System.out.print("No Rekening (kode_No Rekening) : 03");
+                    int rek =ni.nextInt();
+                    System.out.println("An Nama                        : Adik Qu");
+                    System.out.print("Masukkan Nominalnya            : ");
+                    int nom = ni.nextInt();
+                    Jumlah=Saldo - nom;
                     System.out.println("Transfer Sukses !!!");
                 }else{
                     System.out.println("Pilihan Anda Tidak Tersedia !!!");
