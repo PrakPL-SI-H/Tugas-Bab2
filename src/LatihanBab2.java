d0b86fdcfb2da70c4456c57fe9770951311831f5
Nofry <nofryarman@gmail.com>
2016-03-12 12:48:56 -0800

    Open project, membuat class bernama ATM dan membuat variabel global serta constructor nya

--- src/LatihanBab2.java
+++ src/LatihanBab2.java
@@ -1,13 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author irwin deriyan
- */
-public class LatihanBab1 {
+import java.util.Scanner;
+class ATM{
+    String nama,pin, noRek;
+    int saldo ,uang;
+  public ATM(String nama,String pin, int saldo, String noRek){
+       this.nama = nama;
+       this.pin = pin;
+       this.noRek = noRek;
+       this.saldo = saldo ;
+       uang = 0;
+   }
+}
+public class LatihanBab2 {
     public static void main(String[] args) {
         
         
