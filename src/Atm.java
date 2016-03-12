40c4be9666a1dff8fbc926095dfe09c146ff57cb
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-03-12 21:06:00 +0700

    edit class method (baru login)

---
+++ src/Atm.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prakpl;
+
+/**
+ *
+ * @author Prasetyo
+ */
+public class Atm {
+    int noReg;
+    int Saldo;
+    int akSaldo;
+    int Jumlah;    
+
+    public void Saldo(){
+        Saldo = 100000000;
+    }
+}
