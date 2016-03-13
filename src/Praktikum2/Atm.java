0c5df72826981c1b53e695043df0b5a9a020cae2
FarisGenis <faaris513@gmail.com>
2016-03-13 22:22:57 +0700

    Edit Atm Class

--- src/Praktikum2/Atm.java
+++ src/Praktikum2/Atm.java
@@ -1,31 +1,35 @@
-
 package Praktikum2;
+
 public class Atm {
+
     private int password;
     private int saldo = 1000000000;
     private int tarik = 0;
     private int transfer;
     private String rek;
-    
-    public Atm(){
-        
+
+    public Atm() {
+
     }
-    public int password(){
+
+    public int password() {
         return password = 123456;
     }
-    public void cekSaldo(){
+
+    public void cekSaldo() {
         int sisa = saldo - tarik - transfer;
-        System.out.println("saldo anda saat ini = "+sisa);
+        System.out.println("saldo anda saat ini = " + sisa);
     }
-    public void setTarik(int berapa){
+
+    public void setTarik(int berapa) {
         tarik = berapa;
     }
-    
-    public void doTransfer(int berapa){
+
+    public void doTransfer(int berapa) {
         transfer = berapa;
     }
-    
-    public void rekTransfer(String rek){
+
+    public void rekTransfer(String rek) {
         this.rek = rek;
     }
 }
