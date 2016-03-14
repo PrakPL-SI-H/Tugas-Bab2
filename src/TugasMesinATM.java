1476e75afd3bca03922097c9d1063b7233cf29ca
istighfarinba <bafarcorp@gmail.com>
2016-03-14 12:40:50 +0700

    proses awal(membuat konstruktor)

---
+++ src/TugasMesinATM.java
@@ -0,0 +1,40 @@
+
+package tugasmesinatm;
+class Data {
+
+    int pin;
+    Double saldo;
+
+    public Data(int x,Double y) {
+        pin=x;
+        saldo=y;
+    }
+}
+public class TugasMesinATM {
+
+    /**
+     * @author Istighfarin Bahtiar AMry
+     * @ Nim   155150401111129
+     * @ Kelas SI-H
+     */
+    public static void main(String[] args) {
+        Data masuk=new Data(15515040,7000000.0);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+}
