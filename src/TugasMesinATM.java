13f249bbb325c099de2d9a9e4c78c96a8e97aa67
istighfarinba <bafarcorp@gmail.com>
2016-03-14 13:15:34 +0700

    tambahan konstruktor kode pin dan pilihan menu

--- src/TugasMesinATM.java
+++ src/TugasMesinATM.java
@@ -1,40 +1,76 @@
-
 package tugasmesinatm;
+
+import java.util.Scanner;
+
 class Data {
 
     int pin;
     Double saldo;
 
-    public Data(int x,Double y) {
-        pin=x;
-        saldo=y;
+    public Data(int x) {
+        pin = x;
+
+    }
+
+    public void setPin(int password) {
+        this.pin = password;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+//------------------------------------ getter
+    public int getPin() {
+        return pin;
+    }
+
+    public double getSaldo() {
+        return saldo;
     }
 }
+
 public class TugasMesinATM {
 
     /**
      * @author Istighfarin Bahtiar AMry
-     * @ Nim   155150401111129
+     * @ Nim 155150401111129
      * @ Kelas SI-H
      */
     public static void main(String[] args) {
-        Data masuk=new Data(15515040,7000000.0);
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        Scanner in = new Scanner(System.in);
+        Data masuk = new Data(31081996);
+        System.out.println("    =====ATM BAHTIAR JAYA ABADI=====");
+        System.out.println();
+
+        System.out.print("Masukkan PIN Anda  :");
+        int pin = in.nextInt();
+        if (masuk.getPin() == pin) {
+            tampilanMenu();
+        } else {
+            System.out.println();
+            System.out.println("           ===LOGIN GAGAL===");
+            System.out.println("          MAAF PIN ANDA SALAH");
+            System.out.println("            PROGRAM CLOSED");
+
+        }
+
     }
-    
-    
+
+    public static void tampilanMenu() {
+        System.out.println("|=======================================================|");
+
+        System.out.print("|         Selamat Datang di ATM BAHTIAR JAYA ABADI      |");
+        System.out.println();
+        System.out.println("|=======================================================|");
+        System.out.print("|               > PILIH TRANSAKSI ANDA <                |");
+        System.out.println();
+        System.out.print("|   [1] CEK SALDO               [2] TRANSFER UANG       |");
+        System.out.println();
+        System.out.print("|   [3] PENARIKAN UANG          [4] PEMBAYARAN LAIN-LAIN|");
+        System.out.println();
+        System.out.println("|                     [5] Keluar                        |");
+        System.out.println("|=======================================================|");
+    }
+
 }
