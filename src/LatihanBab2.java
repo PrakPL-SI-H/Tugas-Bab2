9587299551ee292f7737c245076dae6c20b8cda4
Muh. Asdar <muh.asdar17@gmail.com>
2016-03-14 15:48:24 +0700

    mengerjakan beberapa bagian pada class

--- src/LatihanBab2.java
+++ src/LatihanBab2.java
@@ -1,17 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author irwin deriyan
- */
-public class LatihanBab1 {
+
+import java.util.Scanner;
+
+public class LatihanBab2 {
+    
     public static void main(String[] args) {
         
+        Scanner in = new Scanner(System.in);
+        Nasabah user = new Nasabah();
+        boolean usr = false;
+        do {
+            System.out.println("Anjungan Tunai Jomblo");
+            System.out.print("No. Rekening= ");
+            String norek = in.next();
+            System.out.print("Pin         = ");
+            String pin = in.next(); //nim
+            for (int i = 0; i < user.nasabah.length; i++) {
+                if ((norek.equals(user.nasabah[i][0]) && pin.equals(user.nasabah[i][1]))) {
+                    System.out.println("=======================================");
+                    if (i == 1) {
+                        user = new Nasabah(1)
+                    }
+                    user.showData(i);
+                    usr = true;
+                } 
+            }
+            if (usr = false){
+                System.out.println("Data Tidak Ditemukan");
+            }
+        } while (user != true);
         
-        
-    }
-    
-}
+        int pilihan;
+        String batas = ("----------------------------------------------------");
+        do {
+            System.out.printf("%.40s\n", batas);
+            System.out.println("1. Cek Saldo");
+            System.out.println("2. Tarik Tunai");
+            System.out.println("3. Transfer");
+            System.out.println("0. Keluar");
+            System.out.print("Masukkan Pilihan Anda : ");
+            pilihan = in.nextInt();
+            System.out.printf("%.40s\n", batas);
+            switch (pilihan) {
+                case 1:
+                    System.out.println("Saldo Anda Saat ini");
+                    user.nsbhproses.showSaldo();
+                    break;
+                case 2:
+                    System.out.println("\tTARIK TUNAI");
+                    System.out.print("Masukan Jumlah Penarukan = ");
+                    user.nsbhproses.saldoKurang(in.nextInt());
+                    System.out.println("Penarikan berhasil");
+                    break;
+                case 3:
+                    System.out.println("\Transfer");
+            }
+            
+        }
