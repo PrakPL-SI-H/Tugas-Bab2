dacb07783432ff3ea4733902e4156698a7f3f3ff
Firdha Sabiilarrasyad <firdhasabil@gmail.com>
2016-03-15 21:15:30 +0700

    percobaan 1 pembuatan main class yang bernama mainAtm, sudah di deklarasikan beberapa variabel

---
+++ src/mainAtm.java
@@ -0,0 +1,20 @@
+package javaapplication10;
+
+import java.util.Scanner;
+
+class mainAtm {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int pilih;
+        int pilihbank;
+        int nomerr = 0;
+        int duit = 0;
+        String jenengb = "";
+        String back;
+
+        
+
+    }
+}
