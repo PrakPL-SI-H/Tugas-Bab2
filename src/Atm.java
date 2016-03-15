9602ce48ff508b2c70a0e5fdc02d56a545a56323
Firdha Sabiilarrasyad <firdhasabil@gmail.com>
2016-03-15 20:05:19 +0700

    percobaan ke 2. pembuatan konstruktor dan method inputan password atm

--- src/Atm.java
+++ src/Atm.java
@@ -16,5 +16,27 @@ public class Atm {
     String namaBank;
     Scanner input = new Scanner(System.in);
 
-  
+   public Atm() {
+        String nm = "BRI";
+        int pass = 264069;
+        password = pass;
+        nama = nm;
+    }
+
+    void cekPassword() {
+
+        int i = 0;
+        do {
+            System.out.print("Masukan password anda : ");
+            userinput1 = input.nextInt();
+            if (i > 1) {
+                System.out.println("Maaf, transaksi anda dibatalkan");
+                System.exit(0);
+            }
+            i++;
+        } while (userinput1 != password);
+    }
+
+
+
 }
