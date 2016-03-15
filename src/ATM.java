1f398701771754ada0696fb570d4c2aefa7851c5
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-03-15 19:26:51 +0700

    membuat method setPassword, MenuATM, dan setMenuATM

---
+++ src/ATM.java
@@ -0,0 +1,62 @@
+package prakproglan;
+public class ATM {
+    
+    int psword = 12345;
+    int pilihan;
+    int saldo;
+    int saldo1;
+    int saldo2;
+    String penarikan;
+    int penarikan1;
+    
+    public void setPassword(int p){
+        if (psword!=this.psword){
+            System.out.println("Password yang anda masukkan salah");
+            System.out.println("Sistem berhenti....");
+        }
+        else 
+            System.out.println();            
+    }
+    
+    public void MenuATM(){
+        System.out.println("   SELAMAT DATANG  ");
+        System.out.println("===================");
+        System.out.println("Pilihan Menu : ");
+        System.out.println("1. Melihat Saldo");
+        System.out.println("2. Penarikan Uang");
+        System.out.println("3. Transfer");
+    }
+    
+    public void setMenuATM(){
+        switch (pilihan){
+            case 1 :
+                System.out.println("Saldo anda tersisa : "+saldo);
+            case 2 :
+                System.out.println("Penarikan uang yang ingin anda lakukan : ");
+                System.out.println("1. Rp250.000,-");
+                System.out.println("2. Rp500.000,-");
+                System.out.println("3. Rp750.000,-");
+                System.out.println("4. Rp1.000.000,-");
+                System.out.println("5. Penarikan dalam jumlah lain");
+                if (penarikan = "1"){
+                    System.out.println("Saldo anda tersisa : "+(saldo-250000));
+                }
+                else if (penarikan = "2"){
+                    System.out.println("Saldo anda tersisa : "+(saldo-500000));
+                }
+                else if (penarikan = "3"){
+                    System.out.println("Saldo anda tersisa : "+(saldo-750000));
+                }
+                else if (penarikan = "4"){
+                    System.out.println("Saldo anda tersisa : "+(saldo-1000000));
+                }
+                else if (penarikan = "5"){
+                    System.out.println("Saldo anda tersisa : "+(saldo-penarikan1));
+                }
+            case 3 :
+                System.out.println("Anda memilih transfer. Pilih menu");
+                System.out.println("1. Transfer Sesama Bank");
+                System.out.println("2. Transfer Antar Bank");
+    }
+}
+}
