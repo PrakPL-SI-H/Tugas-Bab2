78b5c44319a06b53dc4aadb463d4a6995d8419c1
Firdha Sabiilarrasyad <firdhasabil@gmail.com>
2016-03-15 21:23:46 +0700

    percobaan ke 2 di main class mainAtm. membuat menu ATM, masih dalam bentuk sout belum ada code untuk menu atm nya

--- src/mainAtm.java
+++ src/mainAtm.java
@@ -14,7 +14,23 @@ class mainAtm {
         String jenengb = "";
         String back;
 
-        
+        System.out.println("== Selamat datang di ATM FRDHS ==");
+        System.out.println("== ATM ini menggunakan uang pecahan Rp. 50.000,- ==");
+
+        Atm Modul2 = new Atm();
+
+        Modul2.cekPassword();
+
+        int i = 0;
+        do {
+            System.out.println("=== Pilihan Menu ===");
+            System.out.println("1. Penarikan ");
+            System.out.println("2. Informasi saldo");
+            System.out.println("3. Transfer");
+            System.out.println("4. Keluar");
+
+            System.out.print("Masukan pilihan anda       : ");
+            pilih = input.nextInt();
 
     }
 }
