215c41d93eaa718169ca32b05231517fc37de8c9
Firdha Sabiilarrasyad <firdhasabil@gmail.com>
2016-03-15 19:58:03 +0700

    pembuatan class atm yang masih berisi inputan variabel awal seperti saldo awal, nama, password dan lain lain

---
+++ src/Atm.java
@@ -0,0 +1,20 @@
+package javaapplication10;
+
+import java.util.Scanner;
+
+public class Atm {
+
+    private int firstsaldo = 10000000;
+    private String nama;
+    private int password;
+    int userinput1;
+    int userinput2;
+    int penarikan;
+    int nomtransfer;
+    double rek;
+    public int stop = 0;
+    String namaBank;
+    Scanner input = new Scanner(System.in);
+
+  
+}
