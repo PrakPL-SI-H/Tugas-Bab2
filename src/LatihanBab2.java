d2b5d79e6251887419d1a59fba2ae2d754d1e341
pandudamar <pandudpriambodo@gmail.com>
2016-03-15 17:11:39 +0700

    PanduDamarPriambodo_155150401111134_TugasBab2

--- src/LatihanBab2.java
+++ src/LatihanBab2.java
@@ -7,6 +7,45 @@
  *
  * @author irwin deriyan
  */
+import java.util.Scanner;
+
+class atm {
+private double saldo, sisaSaldo, tarik, transfer;
+    public String noRek, kartuATM;
+    public int pilihan, PIN;
+    Scanner input = new Scanner(System.in);
+
+    atm() {
+    }
+
+    atm(int pin, double saldo) {
+        PIN = pin;
+        this.saldo = saldo;
+    }
+
+    public void menu() {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("****************** SELAMAT DATANG ******************");
+        System.err.println("                     ATM BERSAMA                    ");
+        System.out.println("Silahkan Masukkan Kartu ATM Anda :)) _         ");
+        password();
+    }
+
+    public void password() {
+        System.out.print("Masukkan Nomor PIN Anda : ");
+        PIN = input.nextInt();
+        if (PIN == 12345 || PIN == 54321) {
+            menuOpsi();
+        } else {
+            System.err.println("Maaf.");
+            System.err.println("PIN yang Anda masukkan salah.");
+            System.err.println("Silahkan coba lagi.");
+            System.err.println("=====================================================");
+            System.err.println("");
+            password();
+        }
+    }
+
 public class LatihanBab1 {
     public static void main(String[] args) {
         
