892fd9860a567ec574e92e44fc7c09e1eccba1a4
FajarAH <akbaransyah@gmail.com>
2016-03-15 14:53:53 +0700

    Membuat atribut untuk kelas ATM

---
+++ LaporanBab2/src/ATM.java
@@ -0,0 +1,11 @@
+
+public class ATM {
+    private String nama;
+    private String norek;
+    private String pin;
+    private int saldo;
+    public int jumlahTarik;
+    public int jumlahTransfer;
+    
+    
+}
