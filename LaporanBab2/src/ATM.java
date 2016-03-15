dd441ef66e5bd274a90d51ac34dd0b908c7a6821
FajarAH <akbaransyah@gmail.com>
2016-03-16 03:52:19 +0700

    edit source code
    membuat method untuk menarik saldo sesuai pilihan

--- LaporanBab2/src/ATM.java
+++ LaporanBab2/src/ATM.java
@@ -35,6 +35,25 @@ public class ATM {
      public void showSaldo(){
          System.out.println("Saldo Anda Saat Ini Adalah\t: Rp. "+saldo);
      }
+     public void setJumlahTarik (int JT){
+         jumlahTarik = JT;
+     }
+     public int getJumlahTarik (){
+         return jumlahTarik;
+     }
+     public void tarikSaldo(){
+         if (saldo > 50000){
+             if ((saldo - 50000)>=jumlahTarik){
+                 if (jumlahTarik % 50000 == 0){
+                     saldo -= jumlahTarik;
+                     System.out.println("Transaksi Anda Berhasil, Sisa Saldo Anda Adalah Rp. "+saldo);
+                 }else { System.out.println("Maad Anda Gagal Melakakukan Transaksi, Silahkan Coba Lagi Dengan Kelipatan Rp. 50000");
+             }
+         }else { System.out.println("Saldo Anda Kurang Untuk Melakukan Transaksi Ini");
+             }
+     }else { System.out.println("Saldo Anda Kurang Untuk Melakukan Transaksi Ini");
+             }    
+       }
      public void menuPertama (){
          System.out.println("\nPILIH JUMLAH PAKET TUNAI\n");
          System.out.printf("%s %30s\n","1. <= 50.000","500.000 => 5.");
@@ -42,6 +61,33 @@ public class ATM {
          System.out.printf("%s %30s\n","3. <= 200.000","1000.000 => 7.");
          System.out.printf("%s %30s\n","4. <= 300.000","TRANSAKSI LAIN => 8.");
          
+     }
+     public void seleksimenuPertama (int pil1){
+         switch (pil1){
+             case 1 : jumlahTarik = 50000;
+                 tarikSaldo();
+                 break;
+             case 2 : jumlahTarik = 100000;
+                 tarikSaldo();
+                 break;
+             case 3 : jumlahTarik = 200000;
+                 tarikSaldo();
+                 break;
+             case 4 : jumlahTarik = 300000;
+                 tarikSaldo();
+                 break;
+             case 5 : jumlahTarik = 500000;
+                 tarikSaldo();
+                 break;
+             case 6 : jumlahTarik = 750000;
+                 tarikSaldo();
+                 break;
+             case 7 : jumlahTarik = 1000000;
+                 tarikSaldo();
+                 break;
+             default : System.out.println("PILIHAN YANG ANDA MASUKKAN SALAH");
+                 break;
+         }
      }
             
     
