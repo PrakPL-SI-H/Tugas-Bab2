1fe7b3f8154401fb11d971b5f3c6e53cf3be6d3a
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-03-15 21:00:50 +0700

    memindahkan method setmenuATM, cekSaldo, penarikan, transfer ke MainATM.java

--- src/ATM.java
+++ src/ATM.java
@@ -17,49 +17,4 @@ public class ATM {
         else 
             System.out.println();            
     }
-    
-    public void setMenuATM(){
-        switch (pilihan){
-            case 1 :
-                cekSaldo();
-            case 2 :
-                penarikan();
-            case 3 :
-                transfer();
-    }
-    }
-    
-    public void cekSaldo(){
-        System.out.println("Saldo anda tersisa : "+saldo);
-    }
-    
-    public void penarikan(){
-        System.out.println("Penarikan uang yang ingin anda lakukan : ");
-        System.out.println("1. Rp250.000,-");
-        System.out.println("2. Rp500.000,-");
-        System.out.println("3. Rp750.000,-");
-        System.out.println("4. Rp1.000.000,-");
-        System.out.println("5. Penarikan dalam jumlah lain");
-            if (penarikan = "1"){
-                System.out.println("Saldo anda tersisa : "+(saldo-250000));
-            }
-            else if (penarikan = "2"){
-                System.out.println("Saldo anda tersisa : "+(saldo-500000));
-            }
-            else if (penarikan = "3"){
-                System.out.println("Saldo anda tersisa : "+(saldo-750000));
-            }
-            else if (penarikan = "4"){
-                System.out.println("Saldo anda tersisa : "+(saldo-1000000));
-            }
-            else if (penarikan = "5"){
-                System.out.println("Saldo anda tersisa : "+(saldo-penarikan1));
-            }
-    }
-    
-    public void transfer(){
-        System.out.println("Anda memilih transfer. Pilih menu");
-        System.out.println("1. Transfer Sesama Bank");
-        System.out.println("2. Transfer Antar Bank");
-    }
 }
