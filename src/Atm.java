40a15dd3dd6a562c0355ae152255ced419087ddd
muhammad arif fathony <onythree1@yahoo.co.id>
2016-03-16 22:32:36 +0700

    class atm selesai

--- src/Atm.java
+++ src/Atm.java
@@ -1,11 +1,11 @@
 
-package atm;
+package mainatm;
 
 import java.util.Scanner;
 
 
-public class Atm {
-    private double saldo=1000000;
+class Atm {
+     private double saldo=1000000;
     private String nama;
     private String pwd;
     
@@ -13,7 +13,7 @@ public class Atm {
         nama=nm;
         pwd=pas;
     }   
-     private Boolean cekPwd(){
+     public Boolean cekPwd(){
             Scanner input=new Scanner(System.in);
             System.out.print("Masukkan nama :");
             String namai=input.next();
@@ -28,14 +28,45 @@ public class Atm {
                         else{
                         o= false;
          }
-                        return o;
-    }   
-
-
-
-
-   
-       
+                        return o; }
+     
+        public double infoSaldo(){
+                System.out.println("Saldo anda : Rp."+saldo);
+                return saldo;
+    }
+        public double penarikan(){
+            Scanner input=new Scanner(System.in);
+            System.out.print("Masukkan jumlah penarikan : Rp");
+            double tarik=input.nextDouble();
+            double out=0;
+                if (tarik>saldo){
+                    System.out.println("saldo tidak mencukupi");
+        }
+                else{
+                    System.out.println("user telah melakukan penarikan sejumlah Rp."+tarik);
+                saldo-=tarik;
+                out= saldo;
+        }
+                return out;
     }
     
-
+        void transfer(int norek, double jml){
+            if (saldo>jml){
+            saldo-=jml;
+                System.out.println("user telah melakukan transfer sejumlah Rp."+ jml +" pada nomor rekening "+ norek);
+            }
+            else{
+                System.out.println("saldo tidak mencukupi");
+        }
+    }
+        void transfer(int norek, String nmBank, double jml){ 
+            if (saldo>jml){
+            saldo-=jml;
+                System.out.println("user telah melakukan transfer sejumlah Rp."+ jml +" pada nomor rekening "+ norek +" pada bank "+ nmBank);
+    }
+            else{
+                System.out.println("saldo tidak mencukupi");
+                
+            }
+        }
+}
