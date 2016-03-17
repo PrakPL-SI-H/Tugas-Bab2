f2568c803bea999246124c6bdbd4c103a37ba31b
Muhammad Moeslim Al-Rasyid <mrasyid1@yahoo.com>
2016-03-16 20:05:43 -0700

    Pembuatan Class Atm

--- src/LatihanBab2.java
+++ src/LatihanBab2.java
@@ -1,17 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author irwin deriyan
- */
-public class LatihanBab1 {
-    public static void main(String[] args) {
-        
-        
-        
-    }
+import java.util.Scanner;
+
+class ATM{
+    String nama;
+    String pin;
+    String transfer;
+    double saldo, nominal;
+    
+    
+ public ATM (String Nama,String Pin,String Transfer,double Saldo){
+     nama = Nama;
+     pin = Pin;
+     transfer = Transfer;
+     saldo = Saldo;
+     
+     nominal =0;
+ }
+ public void NrSaldo(){
+     System.out.println("Penarikan Saldo");
+     System.out.println("Jumlah Saldo Anda: Rp."+saldo);
+     System.out.println("")
+ }
+ 
+ public void NrNominal ( double Nominal){
+     nominal = Nominal;
+     if (saldo > Nominal){
+         saldo = saldo - nominal;
+         System.out.println("Penarikan uang anda:"+Nominal);
+         System.out.println("Sisa Saldo Anda:"+saldo);
+     }else System.out.println("Saldo Tidak Mencukupi, Transaksi Berakhir");
+ 
+ }
+    
+ public void NrTransfer (){
+     
+ }    
     
 }
+public class LatihanBab2 {
+    
+  
+    public static void main(String[] args) {
+        
