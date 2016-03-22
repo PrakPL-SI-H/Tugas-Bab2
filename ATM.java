eaceaf1d6532d9462b0be59803f0ec623ff4901c
wenigustirahayu <wenigr97@gmail.com>
2016-03-22 12:12:34 +0700

    155150401111140_Weni Gusti Rahayu_TugasBAB2Revisi

---
+++ ATM.java
@@ -0,0 +1,28 @@
+package mainatm;
+public class ATM {
+
+    String nama;
+    int pass;
+    int uang;
+    int rekening;
+
+    public ATM(String a, int b, int c, int d) {
+        nama = a;
+        pass = b;
+        uang = c;
+        rekening = d;
+    }
+
+    public void cekSaldo() {
+        System.out.println("Saldo Anda Saat Ini Adalah Rp." + uang);
+
+    }
+
+    public void penarikan(int a) {
+        uang = uang - a;
+    }
+
+    public void transfer(int a) {
+        uang = uang - a;
+    }
+}
